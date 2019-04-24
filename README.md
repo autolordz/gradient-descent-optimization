76f9ca38df9f33864c96ebc60794b15326858837
autolordz <autolordz@gmail.com>
2019-04-25 01:21:50 +0800

    add L1L2

--- README.md
+++ README.md
@@ -2,26 +2,30 @@
 
 a python script of a function summarize some popular methods about gradient descent
 损失函数为普通最小二乘OLS
-机器学习凸优化练手项目
+机器学习模型与优化求解的练手项目
 Python编写并使用简明的数学符号
 
 ## TOC
 <!-- MarkdownTOC -->
 
-- Updated
-- Features
-- Usage
-- Reference
-- Licence
+- [Updated](#updated)
+- [Features](#features)
+- [Usage](#usage)
+- [Reference](#reference)
+- [Licence](#licence)
 
 <!-- /MarkdownTOC -->
 
-## Updated
+<a id="updated"></a>
+## Updated  
+【2019-4-25】
+添加约束优化L1L2等内容  
+![](./img/gf4.png)
+
+<a id="features"></a>
 ## Features
 
-
-
-- [x] 一阶优化
+- [x] 一阶优化(非约束)
 	- 原版
 		- GD 全批梯度下降 mm10
 		- minibatch-GD 小批梯度下降 mm10
@@ -36,7 +40,7 @@ Python编写并使用简明的数学符号
 	- 共轭梯度法 mm32
 	- 拟牛顿法Quasi-Newton(Broyden) mm33
 - [x] 非凸优化
-	- Adagrad mm40
+	- Adagrad(自适应梯度) mm40
 	- RMSProp mm41
 	- Adadelta mm42
 	- Adam mm43
@@ -51,17 +55,24 @@ Python编写并使用简明的数学符号
 - [x] 速率
 	- Armijo rules mm11
 	- [ ] Wolfe conditions
-- [ ] 约束优化
-	- 稀疏约束
-		- L0 约束
-			- 向前逐步回归(匹配追踪)
-		- L1 约束 Lasso
-			- 软阈值坐标下降
-			- 近端梯度(广义梯度)
-			- 对偶ADMM
-		- L2 约束 岭回归 (同非约束)
-- [ ] 矩阵回归(分解)
+- [x] 约束优化
+	- L0(Best subset selection)
+		- 向前逐步回归(匹配追踪)
+	- L1(Lasso) 
+		- 软阈值坐标下降
+		- 近端梯度(广义梯度)
+		- 对偶ADMM
+	- L2(岭回归)
+	- L2(组Lasso)
+	- L1+L2(弹性网)
+	- L1+L2(稀疏组Lasso)
+- [ ] 矩阵相关
+	- 矩阵回归(分解)
+	- 矩阵稀疏约束
+- [ ] 非凸约束
+	- 自适应Lasso
 
+<a id="usage"></a>
 ## Usage
 
 spyder下直接块运行
@@ -72,6 +83,7 @@ Sample:
 ![](./img/gf2.png)
 ![](./img/gf3.png)
 
+<a id="reference"></a>
 ## Reference
 
 [梯度下降总结](http://ruder.io/optimizing-gradient-descent/index.html#nesterovacceleratedgradient)
@@ -83,10 +95,14 @@ Sample:
 [分布式机器学习:算法、理论与实践-第4、5章](https://item.jd.com/12444377.html)
 [深度学习核心技术与实践-第6章](https://item.jd.com/12316912.html)
 [神经网络设计-第9章](hagan.okstate.edu/nnd.html)
+[[稀疏统计学习及其应用](https://item.jd.com/12296738.html)statistical learning with sparsity](https://web.stanford.edu/~hastie/StatLearnSparsity_files/SLS_corrected_1.4.16.pdf)
+[坐标下降软阈值Lasso推导](https://stats.stackexchange.com/questions/123672/coordinate-descent-soft-thresholding-update-operator-for-lasso)
+[矩阵分析与应用-张贤达-第3、4章](https://book.douban.com/subject/1257113/)
 
+<a id="licence"></a>
 ## Licence
 
-[Go to](#gradient-descent-optimization)
+[MIT Go to](#gradient-descent-optimization)
 
 THE END
 Enjoy
--- gradient_f.py
+++ gradient_f.py
@@ -19,95 +19,152 @@ def gradient_descent_f(w,X,y,n_iters = 10,
     isarmijo = kwargs.get('isarmijo',False)
 
     doplot = True
-    print_range = np.round(np.linspace(1,n_iters,10))
     n = len(y)
     w_h,Δh = [],[]
     
+    # Shuffle X,y 
     np.random.seed(seed=43)
     r = np.random.permutation(n)
     X = X[r,:]
     y = y[r]
     
-    def J_(w):
-        return 1./n*(X.dot(w)-y).dot(X.dot(w)-y)
-    def gJ(w):
-        return 2./n*X.T.dot(X.dot(w)-y)
-    def F(w):
-        return 1./n_batch*(X_b.dot(w)-y_b).dot(X_b.dot(w)-y_b)
-    def gF(w):
-        return 2./n_batch*X_b.T.dot(X_b.dot(w)-y_b)
-    def gFj(w,j):
-        Xj = X_b[:,j]
-        if type(j) == list:
-            Xj = Xj.T # d>1 X transpose
-        return 2./n_batch*Xj.dot(X_b.dot(w)-y_b)
-    def gFj1(w,j):
-        g = X_b[:,j].dot(y_b-X_b.dot(w))/ X_b[:,j].dot(X_b[:,j])
-        g = 2./n_batch*(g+w[j])
-        return g
-    def gFj2(w,j):
-        Xjwj = np.delete(X_b,j,1).dot(np.delete(w,j,0))
-        g = 2./n_batch*X_b[:,j].dot(y_b-Xjwj) / X_b[:,j].dot(X_b[:,j])
-        return g
+    n_b = n_batch
+    
     def armijo(w):
         ŋ = 1;β = .8;α=.5
-        g = gF(w)
-        while F(w-ŋ*g) > F(w) + ŋ*α*g.dot(-g):
+        g = gJb(w)
+        while Jb(w-ŋ*g) > Jb(w) + ŋ*α*g.dot(-g):
             ŋ *= β
         return ŋ
     def B_(): # Hessian
-        return 2./n_batch * X_b.T.dot(X_b)
+        return 2./n_b * X_b.T.dot(X_b)
     def H_(B): # invert H
         return np.linalg.inv(B)
+    def J_(w): # OLS
+        return 1./n*(X.dot(w)-y).dot(X.dot(w)-y)
+    def Jb(w):
+        return 1./n_b*(X_b.dot(w)-y_b).dot(X_b.dot(w)-y_b)
+    def gJ(w): # gradient of OLS
+        return 2./n*X.T.dot(X.dot(w)-y)
+    def gJb(w):
+        return 2./n_b*X_b.T.dot(X_b.dot(w)-y_b)
+    def J_L0(w): # Best subset selection
+        return 1./n*(X.dot(w)-y).dot(X.dot(w)-y) + λ*np.linalg.norm(w,ord=0)
+    def J_L1(w): # Lasso
+        return 1./n*(X.dot(w)-y).dot(X.dot(w)-y) + λ*np.linalg.norm(w,ord=1)
+    def J_L2(w): # Ridge
+        return 1./n*(X.dot(w)-y).dot(X.dot(w)-y) + λ*w.dot(w)
+    def gJ_L2(w):
+        return 2./n_b*X_b.T.dot(X_b.dot(w)-y_b)+2*λ*w
+    def J_GL1(w): # Group Lasso
+        return 1./n*(X.dot(w)-y).dot(X.dot(w)-y) + λ*np.linalg.norm(w,ord=2)
+    def J_L1L2(w): # elnet
+        return 1./n*(X.dot(w)-y).dot(X.dot(w)-y) + \
+                    λ*1/2*((1-α)*w.dot(w)+α*np.linalg.norm(w,ord=1))
+    def group_w(w,n):
+        return np.array_split(np.random.permutation(len(w)),n)
+    def Sλ(x,λ):
+        if x > λ:
+            return x - λ
+        elif x < -λ:
+            return x + λ
+        else:
+            return 0.
+    def gJj(w,j):
+        Xj = X_b[:,j]
+        return 2./n_b*(X_b.dot(w)-y_b).dot(Xj)
+    def Wj(w,j):
+        z_j = X_b[:,j].dot(X_b[:,j])
+        ρ_j = X_b[:,j].dot(y_b-X_b.dot(w))
+        return ρ_j/z_j+w[j]
+    def Wj_L1(w,j,λ):
+        c = 2./n_b
+        zj = X_b[:,j].dot(X_b[:,j])
+        ρj = X_b[:,j].dot(y_b-X_b.dot(w) + X_b[:,j]*w[j])
+        return Sλ(ρj,λ/c)/zj
+    def Wj_L1L2(w,j,λ,α):
+        c = 2./n_b
+        zj = X_b[:,j].dot(X_b[:,j]) + λ*(1-α)
+        ρj = X_b[:,j].dot(y_b-X_b.dot(w) + X_b[:,j]*w[j])
+        return Sλ(ρj,λ/c)/zj
+    
+    λ = 1e-2
+    α = 1.
+    if 'mm51' == func: J_ = J_L1
+    if 'mm52' == func: J_ = J_L1
+    if 'mm53' == func: J_ = J_L2
+    if 'mm54' == func: J_ = J_L1L2
     
     if isbegin:Δh.append(np.hstack([0,J_(w),w.copy()]))
     
     θ = w1 = w.copy()
     B = H = np.eye(len(w))
-    fname = G = m = v = u = 0
+    fname = G = m = v = u = μ = 0
     β1,β2=.9,.999
     s = 1.
     γ = 0.9
     ε = 1e-2
-    ŋ = 1e-1
+    ŋ = ρ = 1e-1
     
-    paras = 'β1,β2=.9,.999 γ = %s ε = 1e-2 ŋ = %s'%('t/(t+3)' if isγ else γ,
-                                                    'armijo' if isarmijo else ŋ)
-    
+    paras = '''β1,β2=.9,.999,γ=%s
+                ρ=1e-1,λ=1e-2,α=1.
+                ε=1e-2 ŋ=%s '''\
+            %('t/(t+3)' if isγ else γ,
+              'armijo' if isarmijo else ŋ)
     for i in range(n_iters):
         t = i+1
         if isγ: γ = t/(t+3)
-        for k in range(0,n,n_batch):
-            X_b = X[k:k + n_batch]
-            y_b = y[k:k + n_batch]
+        for k in range(0,n,n_b):
+            X_b = X[k:k + n_b]
+            y_b = y[k:k + n_b]
             if isarmijo: ŋ = armijo(w)
-            if 'mm21' == func:
+            if 'mm51' == func:
+                fname = 'Ridge'
+                w += -ŋ*gJ_L2(w)
+            elif 'mm53' == func:
+                fname = 'elnet'
+                j = np.mod(i,len(w)) # loop over each weight
+                w[j] = Wj_L1L2(w,j,λ,α)
+            elif 'mm10' == func: # Origin
+                fname = 'SGD'
+                w += -ŋ*gJb(w)
+            elif 'mm52' == func:
+                fname = 'Lasso'
+                j = np.mod(i,len(w)) # loop over each weight
+                w[j] = Wj_L1(w,j,λ)
+            elif 'mm54' == func:
+                fname = 'ADMM'
+                β = np.linalg.inv(X_b.T.dot(X_b) + ρ*np.eye(len(w))).dot(X.T.dot(y)+ρ*θ-μ)
+                θ = Sλ(np.average(β+μ/ρ),λ/ρ/n)
+                μ = μ + ρ*(β-θ)
+                w = β.copy()
+            elif 'mm21' == func:
                 fname = 'Polyak-M-H' # Polyak’s Momentum Head
-                v = γ*v + ŋ*gF(w)
+                v = γ*v + ŋ*gJb(w)
                 w += -v
             elif 'mm22' == func: # Polyak’s Momentum Back
                 fname = 'Polyak-M-B'
-                w2 = w1 - ŋ*gF(w1) + γ*(w1 - w)
+                w2 = w1 - ŋ*gJb(w1) + γ*(w1 - w)
                 w = w1
                 w1 = w2
             elif 'mm23' == func: # NAG Head 1
                 fname = 'NAG-H'
-                v = γ*v + ŋ*gF(w-γ*v)
+                v = γ*v + ŋ*gJb(w-γ*v)
                 w += -v
             elif 'mm24' == func:
                 fname = 'NAG-B' # NAG Back
                 v0 = v
-                v = γ*v - ŋ*gF(w) 
+                v = γ*v - ŋ*gJb(w) 
                 w += v + γ*(v-v0)
             elif 'mm25' == func:
                 fname = 'NAG-H2' #  NAG Head 2
                 w1 = w + γ * v 
-                v = γ*v - ŋ*gF(w1)
+                v = γ*v - ŋ*gJb(w1)
                 w += v
             elif 'mm26' == func: # a proximal gradient version of Nesterov’s 1983 method
                 fname = 'FISTA'
                 s1 = .5*(1 + np.sqrt(1 + 4*s**2))
-                w1 = θ - ŋ*gF(θ)
+                w1 = θ - ŋ*gJb(θ)
                 θ1 = w1 + (s-1.)/s*(w1-w)
                 # if np.linalg.norm(w1-w) <= ε*np.linalg.norm(w):
                 #     break
@@ -117,16 +174,16 @@ def gradient_descent_f(w,X,y,n_iters = 10,
                 w,θ,s = w1,θ1,s1
             elif 'mm30' == func:
                 fname = 'Newton'
-                w += -H_(B_()).dot(gF(w))
+                w += -H_(B_()).dot(gJb(w))
             elif 'mm31' == func:
                 fname = 'Minimizing Along a Line'
-                g = gF(w)
+                g = gJb(w)
                 p = -g
                 ŋ = -g.dot(p) / p.dot(B_()).dot(p)
-                w += -ŋ*gF(w)
+                w += -ŋ*gJb(w)
             elif 'mm32' == func:
                 fname = 'Conjugate Gradient'
-                g = gF(w)
+                g = gJb(w)
                 p = -g
                 if k == 0:
                     β = 0
@@ -137,25 +194,25 @@ def gradient_descent_f(w,X,y,n_iters = 10,
                 g0 = g
             elif 'mm33' == func:
                 fname = 'Quasi-Newton(Broyden)'
-                Δw = -ŋ*H@gF(w)
+                Δw = -ŋ*H@gJb(w)
                 w1 = w + Δw
-                Δg = gF(w1) - gF(w)
+                Δg = gJb(w1) - gJb(w)
                 B = B + np.outer(Δg-B@Δw/Δw.dot(Δw), Δw) 
                 H = H + np.outer(Δw-H@Δg, Δw.T@H) / Δw.dot(H).dot(Δg)
                 w = w1
             elif 'mm40' == func:
                 fname = 'Adagrad'
-                g = gF(w)
+                g = gJb(w)
                 G = G + g*g
                 w += -ŋ/np.sqrt(G+ε)*g
             elif 'mm41' == func:
                 fname = 'RMSProp'
-                g = gF(w)
+                g = gJb(w)
                 G = γ*G + (1-γ)*g*g
                 w += -ŋ/np.sqrt(G+ε)*g
             elif 'mm42' == func:
                 fname = 'Adadelta'
-                g = gF(w)
+                g = gJb(w)
                 G = γ*G + (1-γ)*g*g
                 ŋ = np.sqrt(v+ε)/(np.sqrt(G+ε))
                 u = - ŋ*g
@@ -163,7 +220,7 @@ def gradient_descent_f(w,X,y,n_iters = 10,
                 w += u
             elif 'mm43' == func:
                 fname = 'Adam'
-                g = gF(w)
+                g = gJb(w)
                 m = β1*m+(1-β1)*g
                 v = β2*v+(1-β2)*g*g
                 mh = m/(1-β1**(i+1))
@@ -171,14 +228,14 @@ def gradient_descent_f(w,X,y,n_iters = 10,
                 w += - ŋ/(np.sqrt(vh+ε))*mh
             elif 'mm44' == func:
                 fname = 'AdaMax'
-                g = gF(w)
+                g = gJb(w)
                 m = β1*m+(1-β1)*g
                 u = np.maximum(β2*u,abs(g))
                 mh = m/(1-β1**(i+1))
                 w += -ŋ/(u+ε)*mh
             elif 'mm45' == func:
                 fname = 'Nadam'
-                g = gF(w)
+                g = gJb(w)
                 m = β1*m+(1-β1)*g
                 v = β2*v+(1-β2)*g*g
                 gh = g/(1-β1**(i+1))
@@ -188,40 +245,44 @@ def gradient_descent_f(w,X,y,n_iters = 10,
                 w += -ŋ/(np.sqrt(vh+ε))*mhh
             elif 'mm46' == func:
                 fname = 'AMSGrad'
-                g = gF(w)
+                g = gJb(w)
                 m = β1*m+(1-β1)*g
                 vh = v/(1-β2**(i+1))
                 v = β2*v+(1-β2)*g*g
                 vh = np.maximum(vh,v)
                 w += - ŋ/(np.sqrt(vh+ε))*m
-            elif 'mm90' == func:
-                fname = 'R-Cord gFj step descent'
-                j = np.mod(i,len(w))
-                w[j] += -ŋ*gFj(w,j)
             elif 'mm91' == func:
-                fname = 'R-Cord gFj1 each w[j]'
+                fname = 'R-Cord Wj(w)1 each w[j]'
                 j = np.mod(i,len(w))
-                w[j] = gFj1(w,j)
+                w[j] = Wj(w,j)
             elif 'mm92' == func:
-                fname = 'R-Cord gFj2 each w[j]'
+                fname = 'R-Cord Wj(w)2 each w[j]'
                 j = np.mod(i,len(w))
-                w[j] = gFj2(w,j)
+                w[j] = Wj(w,j)
             elif 'mm93' == func:
                 fname = 'S-Cord random w[j]'
                 j = np.random.randint(0,len(w))
-                w[j] = gFj2(w,j)
+                w[j] = Wj(w,j)
+            elif 'mm90' == func:
+                fname = 'R-Cord gFj step descent'
+                j = np.mod(i,len(w))
+                w[j] += -ŋ*gJj(w,j)
             elif 'mm94' == func:
-                n_b = 2
-                fname = 'SB-Cord batch=%s'%n_b
-                j = np.random.randint(0,len(w),n_b).tolist()
-                w[j] += -ŋ*gFj(w,j)
-            elif 'mm10' == func: # Origin
-                fname = 'SGD'
-                w += -ŋ*gF(w)
+                group_n = 2
+                group = group_w(w,group_n)
+                fname = 'S-R-Block-Cord blocks=%s'%group_n
+                j = group[np.mod(i,group_n)]
+                w[j] += -ŋ*gJj(w,j)
+            elif 'mm95' == func:
+                group_n = 2
+                group = group_w(w,group_n)
+                fname = 'S-S-Block-Cord blocks=%s'%group_n
+                j = group[np.random.randint(0,group_n)]
+                w[j] += -ŋ*gJj(w,j)
             elif 'mm11' == func: # 
                 fname = 'SGD + Armijo rules'
                 ŋ = armijo(w)
-                w += -ŋ*gF(w)
+                w += -ŋ*gJb(w)
             else:
                 doplot = False
             Δh.append(np.hstack([i+k/n,J_(w),w.copy()]))
@@ -229,9 +290,11 @@ def gradient_descent_f(w,X,y,n_iters = 10,
 #    print('==GD=w==\n',w_h)
     return w_h,fname,doplot,paras
 
-def iters_gd_plot(n_batch =1,n_iters = 10,ff=[],
-                 islogy=False,**kwargs):
-    fig, ax = plt.subplots(figsize = (10,10))
+def iters_gd_plot(n_batch =1,
+                  n_iters = 10,
+                  islogy=False,
+                  ff=[],**kwargs):
+    fig, ax = plt.subplots(figsize = (8,8))
     mk = ['.', 'o', 'v', '^', '>', '<', 's', 'p', '*', 'h', 'H', 'D', 'd', '1', '', '']
     aplog = ax.semilogy if islogy else ax.plot
     lastJ = np.Infinity
@@ -268,21 +331,23 @@ y = np.sin(x+noise)
 y_noise = y + noise
 y = y_noise - y_noise.mean()
 w = np.ones(X.shape[1])
-
 w_col = ['w'+str(s) for s in range(len(w))]
+
 #ff = [
-##      'mm90',
-##      'mm41',
-#      'mm94'
+##      'mm54',
+##      'mm52',
+#      'mm521','mm522',
+##      'mm53',
+##      'mm10'
 #      ]
 
-rr = list(range(30,100))
+rr = list(range(50,60))
 ff = ['mm'+str(s) for s in rr]
 iters_gd_plot(ff=ff,
-              n_batch=100,
+              n_batch=20,
               n_iters=30,
               islogy=True,
               isbegin=False,
               isγ=True,
-              isarmijo=False,
+              isarmijo=True,
               )
